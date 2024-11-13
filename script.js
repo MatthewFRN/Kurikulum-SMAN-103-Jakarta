746da6aba582a66c982806c8a17e9084554ac079
MatthewFRN <matthewfrancesco843@gmail.com>
2024-11-13 10:27:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -24,8 +24,7 @@ document.addEventListener("DOMContentLoaded", function() {
             competitiveness: "Tinggi",
             accreditation: "A",
             faculties: ["Fakultas Kedokteran", "Fakultas Hukum", "Fakultas Teknik", "Fakultas Ekonomi"],
-            description: "Universitas terkemuka di Indonesia dengan program studi yang unggul.",
-            website: "<a href='https://www.ui.ac.id/en/universitas-indonesia/'> https://www.ui.ac.id/en/universitas-indonesia/ </a>"
+            description: "Universitas terkemuka di Indonesia dengan program studi yang unggul."
         },
         {
             name: "Institut Teknologi Bandung",
@@ -33,8 +32,7 @@ document.addEventListener("DOMContentLoaded", function() {
             competitiveness: "Sangat Tinggi",
             accreditation: "Unggul",
             faculties: ["Fakultas Teknologi Industri", "Fakultas Teknik Mesin dan Dirgantara", "Sekolah Bisnis dan Manajemen"],
-            description: "Dikenal sebagai pusat pendidikan teknik terbaik di Indonesia.",
-            website: "<a href='https://itb.ac.id/'> https://itb.ac.id/ </a>"
+            description: "Dikenal sebagai pusat pendidikan teknik terbaik di Indonesia."
         },
         {
             name: "Universitas Gadjah Mada",
@@ -42,8 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
             competitiveness: "Tinggi",
             accreditation: "A",
             faculties: ["Fakultas Kedokteran", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Pertanian"],
-            description: "Salah satu universitas tertua dan terbesar di Indonesia.",
-            website: "<a href='https://ugm.ac.id/id/'> https://ugm.ac.id/id/ </a>"
+            description: "Salah satu universitas tertua dan terbesar di Indonesia."
         },
         {
             name: "Institut Teknologi Sepuluh November",
@@ -51,36 +48,53 @@ document.addEventListener("DOMContentLoaded", function() {
             competitiveness: "Tinggi",
             accreditation: "A",
             faculties: ["Fakultas Kedokteran", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Pertanian"],
-            website: "<a href='https://www.its.ac.id/id/beranda/'> https://www.its.ac.id/id/beranda/ </a>"
+            description: "Salah satu universitas tertua dan terbesar di Indonesia."
         },
         { 
-            name: "Universitas Padjadjaran",
-            passingGrade: "653,26",
+        name: "Universitas Veteran Jakarta",
+        passingGrade: "627,85",
+        competitiveness: "Tinggi",
+        accreditation: "A",
+        faculties: ["Fakultas Kedokteran", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Pertanian"],
+        description: "Salah satu universitas tertua dan terbesar di Indonesia."
+        },
+        { 
+            name: "Universitas Veteran Jakarta",
+            passingGrade: "627,85",
             competitiveness: "Tinggi",
             accreditation: "A",
-            faculties: ["Fakultas Hukum", "Fakultas Peternakan dan Pertanian", "Fakultas Psikologi", "Fakultas Ekonomika dan Bisnis", "Fakultas Ilmu Budaya", "Fakultas Perikanan dan Ilmu Kelautan", "Fakultas Teknik", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Sains dan Matematika", "Fakultas Kedokteran"],
-            website: "<a href='https://www.unpad.ac.id/'> https://www.unpad.ac.id/ </a>",
-            description: "Bentuk sumbangsih para penerus bangsa atas amanah yang ditinggalkan para pejuang kemerdekaan"
-        },
-        { 
-            name: "Universitas Negeri Jakarta",
-            passingGrade: "581,74",
-            competitiveness: "Rendah",
-            accreditation: "A",
-            faculties: ["Fakultas Matematika dan IPA. Biologi", "Fakultas Teknik, Keguruan & Pendidikan", "Fakultas Ilmu Pendidikan, Pendidikan Guru Sekolah Dasar", "Fakultas Bahasa dan Seni, Tari", "Fakultas Ilmu Sosial, Sejarah", "Fakultas Ilmu Olahraga, Pendidikan Olahraga", "Fakultas Ekonomi, Ilmu Ekonomi", "Fakultas Pendidikan Psikologi, Psikologi"],
-            website: "<a href='https://www.unj.ac.id/'> https://www.unj.ac.id/ </a>",
-            description: "Universitas Negeri Jakarta (disingkat sebagai UNJ) adalah perguruan tinggi negeri yang terdapat di Kota Jakarta, Indonesia yang didirikan pada tahun 1964."
-        },
-
-           { 
-            name: " Institut Pertanian Bogor",
-            passingGrade: "648,15",
-            competitiveness: "Menengah",
-            accreditation: "A",
-            faculties: ["Fakultas Hukum", "Fakultas Pertanian", "Fakultas Peternakan", "Fakultas Kehutanan dan Lingkungan", "Fakultas Matematika dan Ilmu Pengetahuan Alam", "Fakultas Perikanan dan Ilmu Kelautan", "Fakultas Ekonomi dan Manajemen", "Fakultas Ekologi Manusia"],
-            website: "<a href='https://www.ipb.ac.id/beranda/'> https://www.ipb.ac.id/beranda/ </a>",
-            description: " Institut Pertanian Bogor (IPB) merupakan sebuah perguruan tinggi negeri yang didirikan tanggal 1 September 1963 di Bogor berdasarkan keputusan Menteri Perguruan Tinggi dan Ilmu Pengetahuan (PTIP) No. 92/1963 yang kemudian disahkan oleh Presiden RI Pertama."
-        },
+            faculties: ["Fakultas Kedokteran", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Pertanian"],
+            description: "Salah satu universitas tertua dan terbesar di Indonesia."
+            },
+            { 
+                name: "Universitas Padjadjaran",
+                passingGrade: "653,26",
+                competitiveness: "Tinggi",
+                accreditation: "A",
+                faculties: ["Fakultas Hukum", "Fakultas Peternakan dan Pertanian", "Fakultas Psikologi", "Fakultas Ekonomika dan Bisnis", "Fakultas Ilmu Budaya", "Fakultas Perikanan dan Ilmu Kelautan", "Fakultas Teknik", "Fakultas Ilmu Sosial dan Ilmu Politik", "Fakultas Sains dan Matematika", "Fakultas Kedokteran"],
+                website: "<a href='https://www.unpad.ac.id/'> https://www.unpad.ac.id/ </a>",
+                description: "Bentuk sumbangsih para penerus bangsa atas amanah yang ditinggalkan para pejuang kemerdekaan"
+            },
+            { 
+                name: "Universitas Negeri Jakarta",
+                passingGrade: "581,74",
+                competitiveness: "Rendah",
+                accreditation: "A",
+                faculties: ["Fakultas Matematika dan IPA. Biologi", "Fakultas Teknik, Keguruan & Pendidikan", "Fakultas Ilmu Pendidikan, Pendidikan Guru Sekolah Dasar", "Fakultas Bahasa dan Seni, Tari", "Fakultas Ilmu Sosial, Sejarah", "Fakultas Ilmu Olahraga, Pendidikan Olahraga", "Fakultas Ekonomi, Ilmu Ekonomi", "Fakultas Pendidikan Psikologi, Psikologi"],
+                website: "<a href='https://www.unj.ac.id/'> https://www.unj.ac.id/ </a>",
+                description: "Universitas Negeri Jakarta (disingkat sebagai UNJ) adalah perguruan tinggi negeri yang terdapat di Kota Jakarta, Indonesia yang didirikan pada tahun 1964."
+            },
+    
+               { 
+                name: " Institut Pertanian Bogor",
+                passingGrade: "648,15",
+                competitiveness: "Menengah",
+                accreditation: "A",
+                faculties: ["Fakultas Hukum", "Fakultas Pertanian", "Fakultas Peternakan", "Fakultas Kehutanan dan Lingkungan", "Fakultas Matematika dan Ilmu Pengetahuan Alam", "Fakultas Perikanan dan Ilmu Kelautan", "Fakultas Ekonomi dan Manajemen", "Fakultas Ekologi Manusia"],
+                website: "<a href='https://www.ipb.ac.id/beranda/'> https://www.ipb.ac.id/beranda/ </a>",
+                description: " Institut Pertanian Bogor (IPB) merupakan sebuah perguruan tinggi negeri yang didirikan tanggal 1 September 1963 di Bogor berdasarkan keputusan Menteri Perguruan Tinggi dan Ilmu Pengetahuan (PTIP) No. 92/1963 yang kemudian disahkan oleh Presiden RI Pertama."
+            }
+    
 
     ];
 
@@ -99,7 +113,6 @@ document.addEventListener("DOMContentLoaded", function() {
                 <p><strong>Akreditasi:</strong> ${university.accreditation}</p>
                 <p><strong>Fakultas:</strong> ${university.faculties.join(", ")}</p>
                 <p><strong>Deskripsi:</strong> ${university.description}</p>
-                <p><strong>Website: </strong> ${university.website}</p>
             `;
         } else {
             infoContainer.textContent = "Universitas tidak ditemukan.";
@@ -107,45 +120,33 @@ document.addEventListener("DOMContentLoaded", function() {
     };
 });
 
-function hitungPersentase() {
-    //  nilai dari input form
-    let fisika = parseFloat(document.getElementById("fisika").value) || 0;
-    let kimia = parseFloat(document.getElementById("kimia").value) || 0;
-    let matematika = parseFloat(document.getElementById("matematika").value) || 0;
-    let biologi = parseFloat(document.getElementById("biologi").value) || 0;
-    let informatika = parseFloat(document.getElementById("informatika").value) || 0;
-    let input = document.getElementById("nama");
-    let inputValue = input.value ;
-    let sosiologi = parseFloat(document.getElementById("sosiologi").value) || 0;
-    let geografi = parseFloat(document.getElementById("geografi").value) || 0;
-    let ekonomi = parseFloat(document.getElementById("ekonomi").value) || 0;
+let availableKeywords = [
+    'Institut Teknologi Bandung',
+    'Institut Teknologi Sepuluh November',
+    'Institut Pertanian Bogor',
+    'Universitas Indonesia',
+    'Universitas Gadjah Mada',
+    'Universitas Veteran Jakarta',
+];
 
-    //  rata-rata nilai
-    let rataF1 = (fisika + kimia + matematika + informatika) / 4;
-    let rataF2 = (fisika + kimia + matematika + biologi) / 4;
-    let rataF3 = (informatika + geografi + biologi + ekonomi) / 4;
-    let rataF4 = (informatika + sosiologi + biologi + ekonomi) / 4;
-    let rataF5 = (informatika + sosiologi + ekonomi) / 3;
-    let rataF6 = (informatika + ekonomi + sosiologi) / 3;
-    let rataF7 = (sosiologi + geografi + ekonomi) / 3;
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById(".searchInput");
 
-    //  persentase kelas
-    let F1 = (rataF1 / 100) * 100;
-    let F2 = (rataF2 / 100) * 100;
-    let F3 = (rataF3 / 100) * 100;
-    let F4 = (rataF4 / 100) * 100;
-    let F5 = (rataF5 / 100) * 100;
-    let F6 = (rataF6 / 100) * 100;
-    let F7 = (rataF7 / 100) * 100;
-
-
-    //  persentase
-    document.getElementById("nama1").innerText = "Nama Siswa: " + inputValue
-    document.getElementById("F1").innerText = "F1: " + F1.toFixed(2) + "%";
-    document.getElementById("F2").innerText = "F2: " + F2.toFixed(2) + "%";
-    document.getElementById("F3").innerText = "F3: " + F3.toFixed(2) + "%";
-    document.getElementById("F4").innerText = "F4: " + F4.toFixed(2) + "%";
-    document.getElementById("F5").innerText = "F5: " + F5.toFixed(2) + "%";
-    document.getElementById("F6").innerText = "F6: " + F6.toFixed(2) + "%";
-    document.getElementById("F7").innerText = "F7: " + F7.toFixed(2) + "%";
+inputBox.onkeyup = function () {
+    let result = [];
+    let input = inputBox.value;
+    if(input.length){
+        result = availableKeywords.filter((keyword)=>{
+          return  keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
 }
+
+function display(result){
+    const content = result.map((list)=>{
+        return "<li>" + list ; 
+    });
+}
+
+    resultsBox.innerHTML = "<ul>" + content + "</ul>";
